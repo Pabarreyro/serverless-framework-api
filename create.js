a5222784d92a347c37b93b6c527ed0645923b65b
Pablo Barreyro <Pabarreyro@gmail.com>
2020-04-28 22:15:40 -0700

    Add create lambda

---
+++ create.js
@@ -0,0 +1,35 @@
+import * as uuid from "uuid";
+import handler from './lib/handler';
+import dynamoDb from './lib/dynamoDb';
+
+export const newItem = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    //             Cognito Identity Pool, we will use the identity id
+    //             as the user id of the authenticated user
+    // - 'noteId': a unique uuid
+    // - 'content': parsed from request body
+    // - 'attachment': parsed from request body
+    // - 'createdAt': current Unix timestamp
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDb.put(params);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  };
+
+  return params.Item;
+});
+
