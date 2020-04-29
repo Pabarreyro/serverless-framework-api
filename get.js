b98ea331c8509be11f5a8c7a966bd1db9c403bbb
Pablo Barreyro <Pabarreyro@gmail.com>
2020-04-28 22:17:00 -0700

    Add get lambda

---
+++ get.js
@@ -0,0 +1,27 @@
+import handler from './lib/handler';
+import dynamoDb from './lib/dynamoDb';
+
+export const singleNote = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the note being retrieved
+    // - 'userId': Idetnity Pool identity id of authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    }
+  };
+
+  try {
+    const result = await dynamoDb.get(params);
+  } catch (e) {
+    throw e;
+  }
+
+  if (!result.Item) {
+    throw new Error('Item not found.');
+  }
+
+  return result.Item;
+});
